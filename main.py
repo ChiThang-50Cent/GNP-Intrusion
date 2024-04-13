3ab27332cfccd40f4f4237c65c06e2153362e3f6
ChiThang-50Cent <chithang.nydo@gmail.com>
2024-04-13 13:34:16 +0700

    update

--- main.py
+++ main.py
@@ -12,66 +12,83 @@ from sklearn.ensemble import AdaBoostClassifier
 import argparse
 import numpy as np
 
+
 def main(model):
     training_features, training_labels, test_features, test_labels, costs = load_data()
     data = [training_features, training_labels, test_features, test_labels, costs]
     clf = model
-    
+
     pop = population(20, training_features[0])
-    
+
     fitness_history = []
     for _ in range(100):
         pop = evolve(pop, clf, data)
         pop_fitness = avg_fitness(pop, clf, data)
         fitness_history.append(pop_fitness)
-        
-        print('#'*10, pop_fitness, '#'*10)
-        if pop_fitness <= 30:
+
+        print("#" * 10, pop_fitness, "#" * 10)
+        if pop_fitness <= 250:
             break
 
     print("Avg fitness history: ", fitness_history)
-    
+
     # fittest_results(clf, training_features, training_labels, test_features, test_labels, costs)
-    
+
+
 def individual(length):
     # create an individual, which is binary repr. of selected feature
     return np.random.randint(2, size=len(length))
-    
+
+
 def population(count, length):
     # create 'count' number of individuals
     return [individual(length) for _ in range(count)]
+
 
 def fitness(individual, clf, data):
     # determine the fitness of an individual
     # misclassified class 1 (%) * misclassified class 2 (%) * misclassified class 3 (%) * feature selection cost (resulting value is converted to int)
     # lower is better
     print("\n######################################################################")
-    print("Individual: ", individual)
+    print("Individual: ", list(individual))
 
     fs_cost = feature_selection_cost(individual, data[4])
-    
+
     selected_train_f = get_selected_features(individual, data[0])
     selected_test_f = get_selected_features(individual, data[2])
 
     clf = clf.fit(selected_train_f, data[1])
-    
+
     pred = clf.predict(selected_test_f)
     class_prob = clf.predict_proba(selected_test_f)
     predicted_labels = get_predicted_labels(class_prob)
-    
+
     print(classification_report(data[3], pred))
     print("Mean accuracy: ", clf.score(selected_test_f, data[3]))
-    print("No of correctly classified samples: ", accuracy_score(data[3], predicted_labels, normalize=False))
-    
-    c1_miss_percent, c2_miss_percent = get_class_miss_percentages(data[3], predicted_labels)
-    f_result = int(c1_miss_percent * c2_miss_percent * fs_cost)
-    
-    print ("\nClass accuracies: \n", "class 0: ", (100 - c1_miss_percent), "%\nclass 1: ", (100 - c2_miss_percent), "%\n")
-    print ("Feature selection cost: ", fs_cost)
-    print ("Fitness: ", f_result)
-    print ("######################################################################\n")
-    
+    print(
+        "No of correctly classified samples: ",
+        accuracy_score(data[3], predicted_labels, normalize=False),
+    )
+
+    c1_miss_percent, c2_miss_percent = get_class_miss_percentages(
+        data[3], predicted_labels
+    )
+    f_result = int(c1_miss_percent * c2_miss_percent * (fs_cost // 100))
+
+    print(
+        "\nClass accuracies: \n",
+        "class 0: ",
+        (100 - c1_miss_percent),
+        "%\nclass 1: ",
+        (100 - c2_miss_percent),
+        "%\n",
+    )
+    print("Feature selection cost: ", fs_cost)
+    print("Fitness: ", f_result)
+    print("######################################################################\n")
+
     return f_result
+
 
 def avg_fitness(pop, clf, data):
     # average fitness of a population
@@ -79,72 +96,82 @@ def avg_fitness(pop, clf, data):
     for i in pop:
         tot_fitness += fitness(i, clf, data)
     return tot_fitness / len(pop)
-    
-def evolve(pop, clf, data, retain_percentage=0.50, random_select=0.05, mutate_prob=0.01):
+
+
+def evolve(
+    pop, clf, data, retain_percentage=0.50, random_select=0.05, mutate_prob=0.01
+):
     f_values = [(fitness(i, clf, data), i) for i in pop]
     individuals = [i[1] for i in sorted(f_values, key=lambda x: x[0])]
     retain_length = int(len(pop) * retain_percentage)
     parents = individuals[:retain_length]
-    
+
     # randomly add other individuals to increase diversity
     for i in individuals[retain_length:]:
         if random_select > random():
             parents.append(i)
-            
+
     # mutate
     for i in parents:
         if mutate_prob > random():
             index_to_mutate = randint(0, len(i) - 1)
             i[index_to_mutate] = randint(0, 1)
-    
+
     # crossover
     no_of_parents = len(parents)
     remaining_no_of_ind = len(pop) - no_of_parents
     children = []
-    
+
     while len(children) < remaining_no_of_ind:
         male_index = randint(0, no_of_parents - 1)
         female_index = randint(0, no_of_parents - 1)
-        
+
         if male_index != female_index:
             male = parents[male_index]
             female = parents[female_index]
             half = len(male) // 2
             child = np.concatenate([male[:half], female[half:]])
             children.append(child)
-    
+
     parents.extend(children)
     return parents
-    
+
+
 def feature_selection_cost(selected_features, costs):
     return sum([c for s, c in zip(selected_features, costs) if s == 1])
+
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--model', default='dst', type=str, help='''
+    parser.add_argument(
+        "--model",
+        default="dst",
+        type=str,
+        help="""
     dts -> Decision Tree
     lr -> LogisticRegression
     gnb -> GaussianNB
     knn -> KNeighborsClassifier
     ada -> AdaBoostClassifier
-    ''')
+    """,
+    )
 
     args = parser.parse_args()
 
     model_name = args.model
     model = None
 
-    if model_name == 'dst':
+    if model_name == "dst":
         model = DecisionTreeClassifier()
-    elif model_name == 'lr':
-        model = LogisticRegression(solver='saga')
-    elif model_name == 'gnb':
+    elif model_name == "lr":
+        model = LogisticRegression(solver="saga")
+    elif model_name == "gnb":
         model = GaussianNB()
-    elif model_name == 'knn':
+    elif model_name == "knn":
         model = KNeighborsClassifier()
-    elif model_name == 'ada':
+    elif model_name == "ada":
         model = AdaBoostClassifier()
 
     main(model)
